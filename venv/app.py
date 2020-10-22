4ee2b191e11422cb54d9c6c95baf17b6a7aba2a5
Dhruv <52623929+EasternArtist@users.noreply.github.com>
2020-10-22 17:57:36 +0530

    Update app.py
    alchemy removed

--- venv/app.py
+++ venv/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify, session
 from flask import Response, send_file
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder="template")
 
